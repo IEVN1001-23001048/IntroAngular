c3cbf765260833966bd5202b5863a46d97cc8e1c
IEVN1001-23001048 <84417@alumnos.utleon.edu.mx>
2026-09-21 23:08:28 -0600

    distancia

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { HeroesList } from './heroes/heroes-list/heroes-list';
+import { OperasBas } from './formularios/operas-bas/operas-bas';
+import { Distancia } from './formularios/distancia/distancia';
 
 @NgModule({
-  declarations: [App, HeroesList],
+  declarations: [App, HeroesList, OperasBas, Distancia],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
